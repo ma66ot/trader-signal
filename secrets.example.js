db9c77aabd9a321a2d1acfe8993c45b2fd96da30
ma66ot <kuralt.jan@gmail.com>
2023-06-01 10:17:37 +0200

    Add a secrets example file so it's easier

---
+++ secrets.example.js
@@ -0,0 +1,5 @@
+const apiKey = 'YOUR_BINANCE_API_KEY'; // not needed
+const apiSecret = 'YOUR_BINANCE_API_SECRET'; // not needed
+const mailjsKey = 'YOUR_MAILJS_KEY'; // 'YOUR_MAILJS_KEY'
+const mailjsService = 'YOUR_MAILJS_SERVICE'; // 'YOUR_MAILJS_SERVICE'
+const mailjsTemplate = 'YOUR_MAILJS_TEMPLATE'; // 'YOUR_MAILJS_TEMPLATE'
